bd701ecda425c33739c4c20f99b42d6c5d34ff1c
SageBinder <sagebin13@gmail.com>
2019-10-20 17:18:04 -0700

    Should work with multiple guilds

--- src/Main/BotMain.cs
+++ src/Main/BotMain.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using DSharpPlus;
 using SpoopyViennaBot.Utils;
@@ -9,10 +10,10 @@ namespace SpoopyViennaBot.Main
     internal static class BotMain
     {
         private static DiscordClient _botClient;
-        private static InvokableList _invokables;
+        private static readonly Dictionary<ulong, InvokableList> _guildCommandsDict = new Dictionary<ulong, InvokableList>();
         public static DateTime BotCreateTime;
 
-        private static async Task Main(string[] args)
+        private static async Task Main()
         {
             BotCreateTime = DateTime.UtcNow;
             
@@ -22,14 +23,20 @@ namespace SpoopyViennaBot.Main
             redditApiTimer.AutoReset = true;
             redditApiTimer.Enabled = true;
             
-            _invokables = CommandsInitializer.GetCommandList();
             _botClient = new DiscordClient(new DiscordConfiguration
             {
                 Token = Resources.ReadAllText("discord_token.txt"),
                 TokenType = TokenType.Bot
             });
 
-            _botClient.MessageCreated += async e => await _invokables.Propagate(new MessageContext(_botClient, e)).ConfigureAwait(false);
+            _botClient.MessageCreated += async e =>
+            {
+                if(!_guildCommandsDict.ContainsKey(e.Guild.Id))
+                {
+                    _guildCommandsDict[e.Guild.Id] = CommandsInitializer.GetCommandList();
+                }
+                await _guildCommandsDict[e.Guild.Id].Propagate(new MessageContext(_botClient, e)).ConfigureAwait(false);
+            };
 
             await _botClient.ConnectAsync().ConfigureAwait(false);
             
